e2bab35cb46a96621008f700b336de9241a9bc04
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-26 13:56:20 -0400

    Worked on API routes

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,9 @@ const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
 
+
 const sequelize = require('../config/connection');
+
 
 const seedAll = async() => {
     await sequelize.sync({ force: true });
@@ -11,5 +13,6 @@ const seedAll = async() => {
     await seedComments();
     process.exit(0);
 };
+
 
 seedAll();
